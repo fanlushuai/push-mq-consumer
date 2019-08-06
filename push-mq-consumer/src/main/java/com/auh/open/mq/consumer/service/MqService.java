14aac5a3e8c495c2370587ca5a38c2e6b80dc05d
樊禄帅 <fanlushuai@gozap.com>
2019-08-06 15:17:08 +0800

    调整使用姿势

--- push-mq-consumer/src/main/java/com/auh/open/mq/consumer/service/MqService.java
+++ push-mq-consumer/src/main/java/com/auh/open/mq/consumer/service/MqService.java
@@ -2,7 +2,6 @@ package com.auh.open.mq.consumer.service;
 
 import com.auh.open.mq.common.consts.ExchangeName;
 import com.auh.open.mq.common.consts.RouteKey;
-import com.auh.open.mq.common.dto.push.JacksonMessage;
 import com.auh.open.mq.common.dto.push.PushDTO;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +14,7 @@ public class MqService {
     private RabbitTemplate rabbitTemplate;
 
     public void send(PushDTO pushDTO) {
-        rabbitTemplate.convertAndSend(ExchangeName.TOPIC, RouteKey.PUSH, JacksonMessage.toMessage(pushDTO));
+        rabbitTemplate.convertAndSend(ExchangeName.TOPIC, RouteKey.PUSH, pushDTO);
     }
 
 }
